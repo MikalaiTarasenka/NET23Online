9345889bed3ee47b78ac98ea82557be58db06a23
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-08-17 16:15:27 +0300

    Start rework Core layer.

--- src/AnimalWorld.Data/DependencyInjection.cs
+++ src/AnimalWorld.Data/DependencyInjection.cs
@@ -1,5 +1,9 @@
 ﻿using AnimalWorld.Data.Repositories.Animals;
 using AnimalWorld.Data.Repositories.Interfaces.Animals;
+using AnimalWorld.Data.Repositories.Interfaces.Users;
+using AnimalWorld.Data.Repositories.Interfaces.Zoos;
+using AnimalWorld.Data.Repositories.Users;
+using AnimalWorld.Data.Repositories.Zoos;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +15,12 @@ namespace AnimalWorld.Data
         {
             services.AddDbContext<WebContext>(options => options.UseNpgsql(connectionString));
             services.AddScoped<IAnimalFamilyRepository, AnimalFamilyRepository>();
+            services.AddScoped<IAnimalSpeciesRepository, AnimalSpeciesRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<IPromotionRepository, PromotionRepository>();
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<IZooRepository, ZooRepository>();
             return services;
         }
     }
