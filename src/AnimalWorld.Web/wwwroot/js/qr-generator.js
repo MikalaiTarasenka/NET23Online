0936dbf95e75c4bc7bdb202d6b1e88f229bcc06d
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-09-17 11:25:01 +0300

    Have started pagination.

---
+++ src/AnimalWorld.Web/wwwroot/js/qr-generator.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('.ticket-qr-container').each(function () {
+        var $container = $(this);
+        var qrValue = $container.data('qr-value');
+
+        if (qrValue && typeof QRCode !== 'undefined') {
+            new QRCode(this, {
+                text: qrValue,
+                width: 150,
+                height: 150,
+                colorDark: '#2c3333',
+                colorLight: '#ffffff',
+                correctLevel: QRCode.CorrectLevel.M
+            });
+        }
+    });
+});
